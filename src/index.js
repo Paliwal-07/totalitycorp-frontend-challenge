d2af9603b7583c2c317848e2013c3fcacf8e4c71
Jayans Paliwal <jayans2002@hotmail.com>
2023-10-25 14:47:57 +0530

    added auth

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ import App from './App';
 import { AppProvider } from './context/product_context';
 import { FilterContextProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const domain = process.env.REACT_APP_AUTH_DOMAIN;
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <FilterContextProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterContextProvider>
-    </AppProvider>
+    <Auth0Provider domain={domain} clientId={clientId} onRedirectCallback={window.location.origin}>
+      <AppProvider>
+        <FilterContextProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterContextProvider>
+      </AppProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
